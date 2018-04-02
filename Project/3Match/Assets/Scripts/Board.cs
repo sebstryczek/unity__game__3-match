da6ad381054add13a33b186d54fdcacecdcbb561
Sebastian <sebastian@stryczek.pl>
2018-04-02 11:46:29 +0200

    Create interactive board tiles

---
+++ Project/3Match/Assets/Scripts/Board.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabTile;
+
+    private int lines = 8;
+    private int columns = 6;
+
+    private void Start()
+    {
+        float horizontalOffset = (this.columns - 1.0f) / 2.0f;
+        float verticalOffset = (this.lines - 1.0f) / 2.0f;
+        for (int lineIndex = 0; lineIndex < this.lines; lineIndex++)
+        {
+            for (int columnIndex = 0; columnIndex < this.columns; columnIndex++)
+            {
+                Vector3 pos = new Vector3(columnIndex - horizontalOffset, lineIndex - verticalOffset, 0);
+                GameObject tile = Instantiate(this.prefabTile, pos, Quaternion.identity);
+                tile.transform.SetParent(this.transform);
+            }
+        }
+    }
+    
+    private void Update()
+    {
+        
+    }
+}
---
+++ Project/3Match/Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    private Color defaultColor;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+        this.defaultColor = this.spriteRenderer.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        this.spriteRenderer.material.color = new Color(1, 0, 0, 1);
+    }
+
+    private void OnMouseExit()
+    {
+        this.spriteRenderer.material.color = this.defaultColor;
+    }
+}
